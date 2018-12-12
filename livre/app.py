cb28fb1362d457b2dce532c0b28c895fe41a29c0
Simon Drieux <simon.drieux@orange.fr>
2018-12-12 16:14:43 +0100

    test

--- livre/app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from flask_bootstrap import Bootstrap
-app=Flask(__name__)
-app.config['BOOTSTRAP_SERVE_LOCAL'] = True
-Bootstrap(app)
--- livre/models.py
+++
@@ -1,6 +0,0 @@
-import yaml, os.path
-
-Books = yaml.load(open(os.path.join(os.path.dirname(__file__),"data.yml")))
-
-def get_sample():
-    return Books[0:10]
